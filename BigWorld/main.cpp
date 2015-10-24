e94048287b1faea9f673606c7c116babc758871f
dlively <davidlively@gmail.com>
2015-10-24 14:52:23 -0500

    Initial Commit

---
+++ BigWorld/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  BigWorld
+//
+//  Created by David Lively on 10/24/15.
+//  Copyright (c) 2015 LivelyInteractive. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
