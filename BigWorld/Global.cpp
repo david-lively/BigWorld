e2033abcc8db0b1cae0790cbf7b2df1e8b03ac93
dlively <davidlively@gmail.com>
2015-11-06 12:30:52 -0600

    Re-adding Global.h and Global.cpp, doesn't appear to have changed anything though.

---
+++ BigWorld/Global.cpp
@@ -0,0 +1,9 @@
+//
+//  Global.cpp
+//  BigWorld
+//
+//  Created by David Lively on 11/6/15.
+//  Copyright (c) 2015 LivelyInteractive. All rights reserved.
+//
+
+#include "Global.h"
---
+++ BigWorld/Global.h
@@ -0,0 +1,14 @@
+//
+//  Global.h
+//  BigWorld
+//
+//  Created by David Lively on 11/6/15.
+//  Copyright (c) 2015 LivelyInteractive. All rights reserved.
+//
+
+#ifndef __BigWorld__Global__
+#define __BigWorld__Global__
+
+#include <stdio.h>
+
+#endif /* defined(__BigWorld__Global__) */
